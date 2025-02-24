e20b8758ddb40ecda6d9e80e7bb3891e245ca332
Akshat Mittal <akshatmittal.0007@gmail.com>
2025-02-25 02:05:53 +0700

    adding basic gallery layout

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -1,5 +1,35 @@
+import { StepBack, StepForward } from "lucide-react";
 import Image from "next/image";
 
-export default function About() {
-  return <div className="text-4xl">gllery</div>;
+export default function Gallery() {
+  return (
+    <>
+      <div className="relative col-span-1 border-t-2 border-[#fff]  row-span-3 lg:row-span-6">
+        <Image
+          src="/images/gallery1.png"
+          alt="club"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <div className="relative col-span-1 border-t-2 border-[#fff]  row-span-3 lg:row-span-6">
+        <Image
+          src="/images/gallery1.png"
+          alt="club"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <div className="col-span-1 lg:col-span-2 border-t-2 border-[#fff]  row-span-2 flex justify-between items-end p-4">
+        <StepBack
+          size={40}
+          className="cursor-pointer hover:opacity-55 hover:scale-110"
+        />
+        <StepForward
+          size={40}
+          className="cursor-pointer hover:opacity-55 hover:scale-110"
+        />
+      </div>
+    </>
+  );
 }
