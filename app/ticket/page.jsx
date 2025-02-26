f06d7296e08b991273f42aa1f68bb16c1e6685d9
Akshat Mittal <akshatmittal.0007@gmail.com>
2025-02-26 23:40:26 +0700

    adding layout changes

--- app/ticket/page.jsx
+++ app/ticket/page.jsx
@@ -21,16 +21,16 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="h-full text-[2rem] col col-span-1 border-t border-[#fff] lg:row-span-2 row-span-1">
+      <div className="h-full text-[2rem] col col-span-1 border-t border-[#fff] lg:row-span-2 row-span-2">
         <div className="text-[1rem] pt-2"> date</div>
-        EVENT HEADING
+        Dance Workshop: Learn contemporary moves with professional dance
       </div>
-      <div className="border-t h-full col  border-[#fff] lg:row-span-2 col-span-1 row-span-1">
+      <div className="border-t h-full col  border-[#fff] lg:row-span-2 col-span-1 row-span-2">
         where raw energy meets hypnotic beats. A haven for techno lovers, ROUGE
         is more than just a club; it's an immersive experience of deep bass,
         pulsating lights, and an electrifying atmosphere. Rooted in the
       </div>
-      <div className="h-full text-[2rem] col col-span-1 lg:row-span-3 row-span-1">
+      <div className="h-full text-[2rem] col col-span-1 lg:row-span-3 row-span-3">
         <form className=" pt-4 justify-between flex flex-col gap-4 text-white text-lg">
           <input
             type="text"
